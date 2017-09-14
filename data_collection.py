151457099235b37326fc00aa9132ce145b4be3b5
Karthick <karthick.arya@teachforindia.org>
2017-09-14 22:51:34 +0530

    Key to output Mapping

--- data_collection.py
+++ data_collection.py
@@ -2,6 +2,17 @@ import numpy as np
 import os
 import cv2
 from grabscreen import grab_screen
+from getkeys import key_check
+
+w = [1,0,0,0,0,0,0,0,0]
+s = [0,1,0,0,0,0,0,0,0]
+a = [0,0,1,0,0,0,0,0,0]
+d = [0,0,0,1,0,0,0,0,0]
+wa = [0,0,0,0,1,0,0,0,0]
+wd = [0,0,0,0,0,1,0,0,0]
+sa = [0,0,0,0,0,0,1,0,0]
+sd = [0,0,0,0,0,0,0,1,0]
+nk = [0,0,0,0,0,0,0,0,1]
 
 index=1
 
@@ -15,12 +26,38 @@ while True:
 
 	break
 
+def keyOutput(keys):
+	output = [0,0,0,0,0,0,0,0]
+	if w in keys:
+		output = w
+	elif a in keys:
+		output = a
+	elif s in keys:
+		output = s
+	elif d in keys:
+		output = d
+	elif w in keys and a in keys:
+		output = wa
+	elif w in keys and d in keys:
+		output = wd
+	elif s in keys and a in keys:
+		output = sa
+	elif s in keys and  d in keys:
+		output = sd
+	else
+		output = nk
+
+	return output
+
 def main(file_name, index):
 	training_data = []
 
 	while True:
 		screen = grab_screen(region=(5,30,800, 625))
-		training_data.append(cv2.cvtColor(cv2.resize(screen, (480,270)), cv2.COLOR_BGR2GRAY))
+		screen = cv2.cvtColor(cv2.resize(screen, (480,270)), cv2.COLOR_BGR2GRAY)
+		keys = key_check()
+		output = keyOutput(keys)
+		training_data.append([screen, output])
 #		cv2.imshow("Test", cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 #		if(cv2.waitKey(25) & 0xFF == ord('q')):
 #			cv2.destroyAllWindows()
